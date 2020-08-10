f2c3487e2c22568873428d9bfc6a87400ca9b0fe
anton <uawesome13@gmail.com>
2020-08-10 15:47:42 +0300

    bux fixed
    makemigration

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -8,10 +8,8 @@ done
 
 echo "PostgreSQL started"
 
-python3 manage.py makemigration
+python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
 
-
 exec "$@"
-© 2020 GitHub, Inc.
